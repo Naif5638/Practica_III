4afa2ceffae71c28fd75a5f35461b90a058f922b
Naif5638 <51485239+Naif5638@users.noreply.github.com>
2019-09-25 09:11:22 -0400

    Add project files.

---
+++ Ejercicio_I/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Una empresa tiene dos turnos (mañna y tarde)");
+            Console.WriteLine("********************************************");
+            double[] turnoMatutino = new double[4];
+            double[] turnoVerpertino = new double[4];
+            double sueldoTarde = 0.0, sueldoMan = 0.0;
+            Console.WriteLine("Ingresar sueldo de empleados turno de la mañana: ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Ingresar sueldo: ");
+                turnoMatutino[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nIngresar sueldo de empleados turno de la tarde: ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Ingresar sueldo: ");
+                turnoVerpertino[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n******************************************************");
+            Console.WriteLine("Calculo de Gastos: ");
+            Console.WriteLine("********************************************************");
+            for (int i = 0; i < 4; i++)
+            {
+                sueldoMan += turnoMatutino[i];
+                sueldoTarde += turnoVerpertino[i];
+            }
+
+            Console.WriteLine("Gastos en sueldos del turno de la mañana: {0}", sueldoMan);
+            Console.WriteLine("Gastos en sueldos del turno de la tarde: {0}", sueldoTarde);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio_II/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 4];
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write("Ingrese el valor [{0},{1}]: ", i + 1, j + 1);
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < matriz.GetLength(1); i++)
+            {
+                Console.Write(matriz[matriz.GetLength(0) - matriz.GetLength(0), i]);
+            }
+            
+            Console.WriteLine();
+            
+            for (int i = 0; i < matriz.GetLength(1); i++)
+            {
+                Console.Write(matriz[matriz.GetLength(0) - 1, i]);
+            }
+            
+            Console.WriteLine();
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.WriteLine(matriz[i, matriz.GetLength(0) - matriz.GetLength(0)]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
